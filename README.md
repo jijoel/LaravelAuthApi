555e074a71b9ddd15dd479b02da29d35c115ab44
Joel <jijoel@yahoo.com>
2018-10-25 14:24:40 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# LaravelAuthApi
+
+[![Latest Version on Packagist][ico-version]][link-packagist]
+[![Total Downloads][ico-downloads]][link-downloads]
+[![Build Status][ico-travis]][link-travis]
+[![StyleCI][ico-styleci]][link-styleci]
+
+This is where your description should go. Take a look at [contributing.md](contributing.md) to see a to do list.
+
+## Installation
+
+Via Composer
+
+``` bash
+$ composer require jijoel/laravelauthapi
+```
+
+## Usage
+
+## Change log
+
+Please see the [changelog](changelog.md) for more information on what has changed recently.
+
+## Testing
+
+``` bash
+$ composer test
+```
+
+## Contributing
+
+Please see [contributing.md](contributing.md) for details and a todolist.
+
+## Security
+
+If you discover any security related issues, please email author email instead of using the issue tracker.
+
+## Credits
+
+- [author name][link-author]
+- [All Contributors][link-contributors]
+
+## License
+
+license. Please see the [license file](license.md) for more information.
+
+[ico-version]: https://img.shields.io/packagist/v/jijoel/laravelauthapi.svg?style=flat-square
+[ico-downloads]: https://img.shields.io/packagist/dt/jijoel/laravelauthapi.svg?style=flat-square
+[ico-travis]: https://img.shields.io/travis/jijoel/laravelauthapi/master.svg?style=flat-square
+[ico-styleci]: https://styleci.io/repos/12345678/shield
+
+[link-packagist]: https://packagist.org/packages/jijoel/laravelauthapi
+[link-downloads]: https://packagist.org/packages/jijoel/laravelauthapi
+[link-travis]: https://travis-ci.org/jijoel/laravelauthapi
+[link-styleci]: https://styleci.io/repos/12345678
+[link-author]: https://github.com/jijoel
+[link-contributors]: ../../contributors]
---
+++ src/AuthApiServiceProvider.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Jijoel\AuthApi\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'jijoel');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'jijoel');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
+
+        // Publishing is only necessary when using the CLI.
+        if ($this->app->runningInConsole()) {
+            $this->bootForConsole();
+        }
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/auth-api.php', 'auth-api');
+
+        // Register the service the package provides.
+        // $this->app->singleton('laravel-auth-api', function ($app) {
+        //     return new LaravelAuthApi;
+        // });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['auth-api'];
+    }
+
+    /**
+     * Console-specific booting.
+     *
+     * @return void
+     */
+    protected function bootForConsole()
+    {
+        // Publishing the configuration file.
+        $this->publishes([
+            __DIR__.'/../config/auth-api.php' => config_path('auth-api.php'),
+        ], 'auth-api.config');
+
+        $this->registerMigrations();
+
+        $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
+
+        // Publishing the views.
+        /*$this->publishes([
+            __DIR__.'/../resources/views' => base_path('resources/views/vendor/jijoel'),
+        ], 'laravelauthapi.views');*/
+
+        // Publishing assets.
+        /*$this->publishes([
+            __DIR__.'/../resources/assets' => public_path('vendor/jijoel'),
+        ], 'laravelauthapi.views');*/
+
+        // Publishing the translation files.
+        /*$this->publishes([
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/jijoel'),
+        ], 'laravelauthapi.views');*/
+
+        // Registering package commands.
+        // $this->commands([
+        //     Console\InstallCommand::class,
+        //     Console\ClientCommand::class,
+        //     Console\KeysCommand::class,
+        // ]);
+    }
+
+    /**
+     * Register our migration files.
+     *
+     * @return void
+     */
+    protected function registerMigrations()
+    {
+        return $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        $this->publishes([
+            __DIR__.'/../database/migrations' => database_path('migrations'),
+        ], 'auth-api.migrations');
+    }
+}
