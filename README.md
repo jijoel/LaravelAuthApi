d1ad322fa1022ca3d99b424d5dc7040616d1072e
Joel <jijoel@yahoo.com>
2018-11-15 14:52:54 -0700

    create console command to make javascript scaffolding

--- README.md
+++ README.md
@@ -1,21 +1,76 @@
-# LaravelAuthApi
+Laravel Auth Api
+=====================
 
 [![Latest Version on Packagist][ico-version]][link-packagist]
 [![Total Downloads][ico-downloads]][link-downloads]
 [![Build Status][ico-travis]][link-travis]
 [![StyleCI][ico-styleci]][link-styleci]
 
-This is where your description should go. Take a look at [contributing.md](contributing.md) to see a to do list.
+Use an api to authenticate users.
+
 
 ## Installation
 
 Via Composer
 
 ``` bash
-$ composer require jijoel/laravelauthapi
+$ composer require jijoel/laravel-auth-api
+```
+
+This package works in conjunction with a front-end component pack, based on [Vuetify](https://vuetify.com). To install the front-end components:
+
+``` bash
+$ yarn add vuetify-api-auth
+```
+
+To scaffold the authentication controllers:
+
+``` bash
+$ php artisan make:api-auth
+```
+
+To scaffold front-end components:
+
+``` bash
+$ php artisan make:api-js
+```
+
+## Configuration
+
+To automatically generate routes, include this in `routes/api.php`:
+
+``` php
+AuthApi::routes();
+ApiAuth::socialRoutes();      // oauth routes
+
+ApiAuth::routes(['social']);  // include oauth routes
+```
+
+Add a relationship to linked social accounts to your user model:
+
+``` php
+public function linkedSocialAccounts()
+{
+    return $this->hasMany('Jijoel\AuthApi\LinkedSocialAccount');
+}
+```
+
+Specify a list of user-selectable language translations for your site by creating a list of locales in `config/app.php`:
+
+``` php
+'locale' => 'en',
+
+'locales' => ['en','es'],   // English and Spanish
 ```
 
 ## Usage
+
+`php artisan make:api-auth`
+: Generate authentication controllers
+
+`php artisan make:api-js`
+: Generate front-end javascript files based on vuetify.js
+
 
 ## Change log
 
@@ -33,25 +88,25 @@ Please see [contributing.md](contributing.md) for details and a todolist.
 
 ## Security
 
-If you discover any security related issues, please email author email instead of using the issue tracker.
+If you discover any security related issues, please [email](mailto://jijoel@yahoo.com) instead of using the issue tracker.
 
 ## Credits
 
-- [author name][link-author]
+- [Author][link-author]
 - [All Contributors][link-contributors]
 
 ## License
 
 license. Please see the [license file](license.md) for more information.
 
-[ico-version]: https://img.shields.io/packagist/v/jijoel/laravelauthapi.svg?style=flat-square
-[ico-downloads]: https://img.shields.io/packagist/dt/jijoel/laravelauthapi.svg?style=flat-square
-[ico-travis]: https://img.shields.io/travis/jijoel/laravelauthapi/master.svg?style=flat-square
+[ico-version]: https://img.shields.io/packagist/v/jijoel/laravel-auth-api.svg?style=flat-square
+[ico-downloads]: https://img.shields.io/packagist/dt/jijoel/laravel-auth-api.svg?style=flat-square
+[ico-travis]: https://img.shields.io/travis/jijoel/laravel-auth-api/master.svg?style=flat-square
 [ico-styleci]: https://styleci.io/repos/12345678/shield
 
-[link-packagist]: https://packagist.org/packages/jijoel/laravelauthapi
-[link-downloads]: https://packagist.org/packages/jijoel/laravelauthapi
-[link-travis]: https://travis-ci.org/jijoel/laravelauthapi
+[link-packagist]: https://packagist.org/packages/jijoel/laravel-auth-api
+[link-downloads]: https://packagist.org/packages/jijoel/laravel-auth-api
+[link-travis]: https://travis-ci.org/jijoel/laravel-auth-api
 [link-styleci]: https://styleci.io/repos/12345678
 [link-author]: https://github.com/jijoel
-[link-contributors]: ../../contributors]
+[link-contributors]: contributors.md
--- src/Console/MakeJsCommand.php
+++ src/Console/MakeJsCommand.php
@@ -4,6 +4,7 @@ namespace Jijoel\AuthApi\Console;
 
 use Illuminate\Console\Command;
 
+
 class MakeJsCommand extends Command
 {
     /**
@@ -11,7 +12,20 @@ class MakeJsCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'make:api-js';
+    protected $signature = 'make:api-js
+        {--all : Publish all javascript files for customization }
+        {--components : Publish components for customization }
+        {--forms : Publish forms for customization }
+        {--middleware : Publish middleware for customization }
+        {--layouts : Publish layouts for customization }
+        {--mixins : Publish mixins for customization }
+        {--pages : Publish pages for customization }
+        {--plugins : Publish plugins for customization [default] }
+        {--routes : Publish route files for customization [default] }
+        {--store : Publish store files for customization [default] }
+
+        {--force : Overwrite existing files by default }
+        {--framework=vuetify-api-auth : Use the specified framework }';
 
     /**
      * The console command description.
@@ -21,6 +35,30 @@ class MakeJsCommand extends Command
     protected $description = 'Scaffold front-end resources for api authentication';
 
     /**
+     * The default directories to copy in full
+     */
+    protected $defaults = [
+        'plugins',
+        'routes',
+        'store',
+    ];
+
+    /**
+     * The directories and files to copy
+     */
+    protected $directories = [
+        'components',
+        'forms',
+        'middleware',
+        'layouts',
+        'mixins',
+        'pages',
+        'plugins',
+        'routes',
+        'store',
+    ];
+
+    /**
      * Execute the console command.
      *
      * @param  \Laravel\Passport\ClientRepository  $clients
@@ -28,9 +66,153 @@ class MakeJsCommand extends Command
      */
     public function handle()
     {
-        $this->info('Handling make js');
-        $this->line('<comment>This feature has not been implemented yet</comment>');
-        return;
+        $this->createDirectories();
+        $this->copyFiles();
+
+        $this->info('Javascript scaffolding for API Authentication generated successfully.');
+    }
+
+    /**
+     * Create the directories for the files.
+     *
+     * @return void
+     */
+    protected function createDirectories()
+    {
+        foreach($this->directories as $dir)
+            $this->createDirectory($dir);
+    }
+
+    /**
+     * Create a single directory
+     *
+     * @param  String $dir   directory to create
+     *
+     * @return null
+     */
+    protected function createDirectory($dir)
+    {
+        if (! is_dir($directory = resource_path('js/'.$dir)))
+            mkdir($directory, 0755, true);
+    }
+
+
+    /**
+     * Copy files to the directories
+     *
+     * @return null
+     */
+    protected function copyFiles()
+    {
+        foreach($this->directories as $dir) {
+            $this->createDirectory($dir);
+
+            if (! $this->includeFiles($dir))
+                continue;
+
+            $files = $this->getFileList($this->getSrc($dir));
+            foreach($files as $file) {
+                $this->copyFile($dir, $file);
+            }
+        }
+    }
+
+    /**
+     * Return a list of all files in the given directory
+     *
+     * @param  String $dir  absolute path to desired directory
+     * @return Array        list of visible files in the directory
+     */
+    protected function getFileList($dir)
+    {
+        $list = scandir($dir);
+
+        return array_where($list, function($a) use ($dir) {
+            return ($a[0] !== '.')
+                && is_file($dir.'/'.$a);
+        });
+    }
+
+    /**
+     * Return the source path for files,
+     * based on given framework
+     *
+     * @param  string $path [description]
+     * @return [type]       [description]
+     */
+    protected function getSrc($path='')
+    {
+        $framework = $this->option('framework');
+
+        return base_path("node_modules/$framework/src/$path");
+    }
+
+    /**
+     * Return the destination path for files
+     */
+    protected function getDest($path='')
+    {
+        return resource_path("js/$path");
+    }
+
+    /**
+     * Should we include files from this directory?
+     */
+    protected function includeFiles($dir)
+    {
+        // If they're explicitly called, include them
+        if ($this->option($dir) || $this->option('all'))
+            return true;
+
+        // If specific directories are called, don't include others
+        if (! $this->useDefaultDirectories())
+            return false;
+
+        // Include any of the defaults
+        return in_array($dir, $this->defaults);
+    }
+
+    /**
+     * Should we ONLY use default directories?
+     * (only do this if no directories have been specified)
+     *
+     * @return Boolean
+     */
+    protected function useDefaultDirectories()
+    {
+        if ($this->option('all'))
+            return false;
+
+        $options = $this->options();
+        foreach($this->directories as $opt)
+            if (isset($options[$opt]) && $options[$opt])
+                return false;
+
+        return true;
+    }
+
+    /**
+     * Copy a file from the stub location to the app location.
+     * Set the namespace to the application namespace when copying.
+     *
+     * @param  String $dir  The file is in this directory
+     * @param  String $file The name of the file
+     *
+     * @return null
+     */
+    protected function copyFile($dir, $file)
+    {
+        $src = $this->getSrc("$dir/$file");
+        $dest = $this->getDest("$dir/$file");
+
+        if (file_exists($dest) && ! $this->option('force')) {
+            if (! $this->confirm("The file [{$file}] already exists. Replace it?")) {
+                return;
+            }
+        }
+
+        $data = file_get_contents($src);
+        file_put_contents($dest, $data);
     }
 
 }
